28e8bc57ee8ec7dbdb6f6c7e98676d12bbe721d2
casperOne <casperOne@caspershouse.com>
2017-03-02 17:23:55 -0500

    Initial commit.

---
+++ Ofl.YouTube.DependencyInjection.Autofac/DefaultModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Autofac;
+using Ofl.YouTube.V3;
+
+namespace Ofl.YouTube.DependencyInjection.Autofac
+{
+    public class DefaultModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Validate parameters.
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            // Bind.
+            builder.RegisterType<YouTubeClient>().As<IYouTubeClient>();
+            builder.RegisterType<YouTubeUtilities>().As<IYouTubeUtilities>();
+        }
+    }
+}
