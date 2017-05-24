8fac219689e642bc323593c49a1f16f5af309ef6
casperOne <casperOne@caspershouse.com>
2017-05-24 16:11:25 -0400

    Removed dependency on Ofl.Core.

--- Ofl.YouTube.DependencyInjection.Autofac/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Ofl.YouTube.DependencyInjection.Autofac")]
-[assembly: AssemblyDescription("Ofl.YouTube.Contract implementation registrations in Autofac.")]
-[assembly: AssemblyProduct("Ofl.YouTube.DependencyInjection.Autofac")]
